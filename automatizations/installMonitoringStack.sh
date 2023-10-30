c387b736815bd5ecb4babbd70b764253b63afda3
josemarjp <48124531+josemarjp@users.noreply.github.com>
2023-10-30 14:06:56 -0300

    add coments

--- automatizations/installMonitoringStack.sh
+++ automatizations/installMonitoringStack.sh
@@ -5,30 +5,30 @@ echo "*                     SOLUÇÕES EM TI                  *"
 echo "*                                                     *"
 echo "*******************************************************"
 
-echo Este script irá instalar e configurar o Docker - Grafana, MySQL e Zabbix.
+echo Instaling and configuring Docker - Grafana, MySQL e Zabbix.
 
 
 #!/bin/bash
 
-# Instalação das dependências
+# Instalin dependences.
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
-# Adiciona a chave GPG do repositório do Docker
+# Add GPG key from Docker repository
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-# Adiciona o repositório do Docker ao sistema
+# Add the Docker repository to the system
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-# Atualiza a lista de pacotes e instala o Docker
+# Update the package list and install Docker
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Inicia e habilita o serviço do Docker
+# Starts and enables the Docker service
 systemctl start docker
 systemctl enable docker
 
-# Inicia os contêineres do Grafana, MySQL e Zabbix
+# Exec Grafana, MySQL and Zabbix containers
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.9.3
 docker run -d -p 3000:3000 --name grafana grafana/grafana-enterprise
 docker run --name mysql-server -t -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="zabbix" -e MYSQL_ROOT_PASSWORD="zabbix" -d mysql:8.0.30 --character-set-server=utf8 --collation-server=utf8_bin --default-authentication-plugin=mysql_native_password
@@ -37,4 +37,4 @@ docker run --name zabbix-server -t -e DB_SERVER_HOST="mysql-server" -e MYSQL_DAT
 docker run -d -p 10050:10050 --name zabbix-agent --link mysql-server:mysql --link zabbix-server:zabbix-server -e ZBX_HOSTNAME="Zabbix server" -e ZBX_SERVER_HOST="zabbix-server" -d zabbix/zabbix-agent
 docker run --name zabbix-web-nginx-mysql -t -e DB_SERVER_HOST="mysql-server" -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="zabbix" -e MYSQL_ROOT_PASSWORD="zabbix" --link mysql-server:mysql -p 80:8080 --restart unless-stopped -d zabbix/zabbix-web-nginx-mysql
 
-echo "Docker foi instalado e configurado com sucesso. Contêineres do Grafana, MySQL e Zabbix também foram iniciados."
+echo "Docker has been successfully installed and configured. Grafana, MySQL and Zabbix containers were also launched."
